fba1fe97fed408f1309835cf7b6f91daf2fa2ffe
Conor Nevin <conor.nevin@couchbase.com>
2021-12-28 18:27:30 +0000

    add create package map test

---
+++ imports_test.go
@@ -0,0 +1,32 @@
+package traceable
+
+import (
+	"testing"
+
+	qt "github.com/frankban/quicktest"
+)
+
+func Test_createPackageMap(t *testing.T) {
+	tests := []struct {
+		name        string
+		importPath  string
+		wantPackage string
+	}{
+		{"standard library", "context", "context"},
+	}
+
+	importPaths := make([]string, len(tests))
+	for i := range importPaths {
+		importPaths[i] = tests[i].importPath
+	}
+
+	packageMap, err := createPackageMap(importPaths)
+	qt.Assert(t, err, qt.IsNil)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotPackageName, gotOk := packageMap[tt.importPath]
+			qt.Assert(t, gotOk, qt.IsTrue)
+			qt.Check(t, gotPackageName, qt.Equals, tt.wantPackage)
+		})
+	}
+}
