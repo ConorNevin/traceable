e74dca84e03cb81bc1dca86641e474ac858c0392
Conor Nevin <conor.nevin@couchbase.com>
2021-12-31 21:42:24 +0000

    updates test to call parsePackage

--- parser_test.go
+++ parser_test.go
@@ -2,11 +2,10 @@ package traceable
 
 import (
 	"go/ast"
-	goparser "go/parser"
-	"go/token"
 	"testing"
 
 	qt "github.com/frankban/quicktest"
+	"golang.org/x/tools/go/packages"
 )
 
 func Test_parser_parsePackage(t *testing.T) {
@@ -16,21 +15,30 @@ func Test_parser_parsePackage(t *testing.T) {
 		otherInterfaces:    make(map[string]map[string]*ast.InterfaceType),
 	}
 
-	fs := token.NewFileSet()
-	file, err := goparser.ParseFile(fs, "internal/testdata/geometry/geometry.go", nil, 0)
-	qt.Assert(t, err, qt.IsNil)
+	c := qt.New(t)
 
-	interfaces, err := pp.parseFile("", file)
-	qt.Assert(t, err, qt.IsNil)
-	qt.Assert(t, interfaces, qt.HasLen, 1)
+	cfg := &packages.Config{
+		Mode: packages.NeedName |
+			packages.NeedTypesInfo |
+			packages.NeedSyntax |
+			packages.NeedTypes,
+	}
+	pkgs, err := packages.Load(cfg, "github.com/ConorNevin/traceable/internal/testdata/geometry")
+	c.Assert(err, qt.IsNil)
+	c.Assert(pkgs, qt.HasLen, 1)
 
-	i := interfaces[0]
-	qt.Check(t, i.name, qt.Equals, "Geometry")
-	qt.Assert(t, i.methods, qt.HasLen, 1)
-	qt.Check(t, i.methods[0].name, qt.Equals, "Area")
-	qt.Assert(t, i.methods[0].args, qt.HasLen, 1)
-	qt.Check(t, i.methods[0].args[0].String(), qt.Equals, "context.Context")
-	qt.Assert(t, i.methods[0].returns, qt.HasLen, 2)
-	qt.Check(t, i.methods[0].returns[0].String(), qt.Equals, "float64")
-	qt.Check(t, i.methods[0].returns[1].String(), qt.Equals, "error")
+	pkg, err := pp.parsePackage(pkgs[0])
+	c.Assert(err, qt.IsNil)
+	c.Check(pkg.name, qt.Equals, "geometry")
+	c.Assert(pkg.interfaces, qt.HasLen, 1)
+
+	i := pkg.interfaces[0]
+	c.Check(i.name, qt.Equals, "Geometry")
+	c.Assert(i.methods, qt.HasLen, 1)
+	c.Check(i.methods[0].name, qt.Equals, "Area")
+	c.Assert(i.methods[0].args, qt.HasLen, 1)
+	c.Check(i.methods[0].args[0].String(), qt.Equals, "context.Context")
+	c.Assert(i.methods[0].returns, qt.HasLen, 2)
+	c.Check(i.methods[0].returns[0].String(), qt.Equals, "float64")
+	c.Check(i.methods[0].returns[1].String(), qt.Equals, "error")
 }
