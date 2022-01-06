e8116bfc1ce922f772ee200eb8414d64d3341154
Conor Nevin <conor.nevin@couchbase.com>
2022-01-06 17:43:59 +0000

    detect imports used by interface

--- method.go
+++ method.go
@@ -36,6 +36,53 @@ func (m Method) contextArg() string {
 	return ""
 }
 
+func (m Method) imports() map[string]struct{} {
+	imports := make(map[string]struct{})
+	for _, t := range m.args {
+		for ip := range importsOf(t) {
+			imports[ip] = struct{}{}
+		}
+	}
+	for _, t := range m.returns {
+		for ip := range importsOf(t) {
+			imports[ip] = struct{}{}
+		}
+	}
+
+	return imports
+}
+
+func importsOf(t types.Type) map[string]struct{} {
+	imports := make(map[string]struct{})
+
+	switch u := t.(type) {
+	case *types.Pointer:
+		imports = mergeMaps(imports, importsOf(u.Elem()))
+	case *types.Map:
+		imports = mergeMaps(imports, importsOf(u.Key()), importsOf(u.Elem()))
+	case *types.Array:
+		imports = mergeMaps(imports, importsOf(u.Elem()))
+	case *types.Slice:
+		imports = mergeMaps(imports, importsOf(u.Elem()))
+	case *types.Chan:
+		imports = mergeMaps(imports, importsOf(u.Elem()))
+	case *types.Signature:
+		for i := 0; i < u.Params().Len(); i++ {
+			imports[u.Params().At(i).Pkg().Path()] = struct{}{}
+		}
+		for i := 0; i < u.Results().Len(); i++ {
+			imports[u.Results().At(i).Pkg().Path()] = struct{}{}
+		}
+	case *types.Named:
+		pkg := u.Obj().Pkg()
+		if pkg != nil {
+			imports[pkg.Path()] = struct{}{}
+		}
+	}
+
+	return imports
+}
+
 func isContextType(t types.Type) bool {
 	named, ok := t.(*types.Named)
 	if !ok {
@@ -43,4 +90,15 @@ func isContextType(t types.Type) bool {
 	}
 
 	return named.Obj().Name() == "Context"
+}
+
+func mergeMaps(a map[string]struct{}, maps ...map[string]struct{}) map[string]struct{} {
+	for _, b := range maps {
+		for bk, bv := range b {
+			if _, ok := a[bk]; !ok {
+				a[bk] = bv
+			}
+		}
+	}
+	return a
 }
